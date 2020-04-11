6cdad4296ba6e225fd641efab5232b7955648a9a
Jeff Hildebrandt <jefreyhildebrandt@gmail.com>
2020-04-11 15:19:34 -0400

    simple_agent.py now is an example which uses a conv network and accepts the map screen, using the hit and run map.  Updated hit_and_run.py to use the hit and run map.  Added launch.json

--- hit_and_run.py
+++ hit_and_run.py
@@ -1,4 +1,8 @@
 # A bot trained using reinforcement learning
+# https://github.com/SoyGema/Startcraft_pysc2_minigames
+# https://gamescapad.es/building-bots-in-starcraft-2-for-psychologists/#installation
+# had to add minimap to pysc2\maps\mini_games.py
+# /home/jeff/anaconda3/envs/starcraft_test/lib/python3.6/site-packages/pysc2/
 from pysc2.agents import base_agent
 from pysc2.env import sc2_env
 from pysc2.lib import actions, features, units
@@ -16,12 +20,12 @@ def main(unused_argv):
     try:
         while True:
             with sc2_env.SC2Env(
-                map_name="AbyssalReef",
+                map_name="HitAndRun",
                 players=[sc2_env.Agent(sc2_env.Race.zerg),
                         sc2_env.Bot(sc2_env.Race.random,
                                     sc2_env.Difficulty.very_easy)],
                 agent_interface_format=features.AgentInterfaceFormat(
-                    feature_dimensions=features.Dimensions(screen=84, minimap=64),
+                    feature_dimensions=features.Dimensions(screen=64, minimap=32),
                     use_feature_units=True),
                 #   step_mul=16,
                 game_steps_per_episode=0,
